c55b8d23b7f2b1410c475c229fa3f65514fd07c7
jakins177@email.com <jakins177@email.com>
2020-10-23 12:40:35 -0400

    implements more routes and components such as contact,faq,support, and share

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './components/TestsMap'
@@ -20,6 +20,12 @@ function App() {
             
               <Link to="/">Home</Link> | 
               <Link to="/about">About</Link> |
+              <Link to="/contact">Contact</Link> |
+              <Link to="/faq">FAQ</Link> |
+              <Link to="/support">Support</Link> |
+              <Link to="/share">Share</Link> |
+              
+
               
 
             </nav>
@@ -31,6 +37,18 @@ function App() {
             </Route>
             <Route path="/about">
               <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/faq">
+              <FAQ />
+            </Route>
+            <Route path="/support">
+              <Support />
+            </Route>
+            <Route path="/share">
+              <Share />
             </Route>
           
             
@@ -62,9 +80,65 @@ function About() {
     <h2>About</h2>
   
    
-    <h3>Do you believe that you or someone you know might have Covid-19. This site is all about finding 
-      your nearest testing facilty! Covid-19 testing is completely free in the United States.
+    <h3>Do you believe that you or someone you know might have Covid-19? This site is all about finding 
+      your nearest testing facilty. Covid-19 testing is completely free in the United States.
     </h3>
     
      </div>);
+}
+
+function Contact() {
+  return (<div>
+  
+    <h2>Contact</h2>
+  
+   
+    <h3>Questions? Comments? Concerns?
+    </h3>
+
+    <h3>Email: contact@coivdtestingnearme.com</h3>
+    
+     </div>);
+}
+
+function FAQ() {
+  return (<div>
+  
+    <h2>FAQ</h2>
+  
+   
+    <h3>How can I find a place near me to be tested for Covid-19? <br/>
+      <i>Navigate to home and simply type in your city or zipcode and location markers for the facilities you can visit will appear.</i>
+    </h3>
+
+    <h3>How can I find free testing?<br/>
+      <i>By law Covid-19 testing is free in the United States. Check the laws for your country</i>
+    </h3>
+    
+     </div>);
+}
+
+function Support() {
+  return (<div>
+  
+    <h2>Support</h2>
+  
+   
+    <h3>See a bug or technical issue?</h3>
+     
+
+    <h3>Email: support@coivdtestingnearme.com</h3>
+    
+     </div>);
+}
+
+
+ class Share extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Share this site on social media. Facebook, Instagram, Twitter</h3>
+      </div>
+    )
+  }
 }
